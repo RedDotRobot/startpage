97e55a5905223d540f3341c9b82d42738e617205
RedDotRobot <88494140+RedDotRobot@users.noreply.github.com>
2024-01-29 18:36:23 +1100

    Update shortcuts.js

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -1,44 +1,21 @@
 export default [
   {
+    category: "School",
+    color: "red",
+    items: {
+      Gmail: "https://mail.google.com/mail/u/0/#inbox",
+      Google_Classroom: "https://classroom.google.com/u/0/",
+      Sentral: "https://fortstreeths.sentral.com.au/s-068p65/portal2/#!/student/3172",
+      Notion: "https://notion.so",
+    },
+  },
+  {
     category: "Dev",
     color: "cyan",
     items: {
       Github: "https://github.com",
-      AWS: "https://aws.amazon.com",
-      "Dev.to": "https://dev.to",
-      Firebase: "https://console.firebase.google.com/u/0",
+      VSCode: "https://vscode.dev",
+      StackOverflow: "https://stackoverflow.com",
     },
-  },
-  {
-    category: "School",
-    color: "red",
-    items: {
-      Outlook: "https://outlook.office365.com/mail/inbox",
-      Canvas: "https://sit.instructure.com",
-      MyStevens: "https://login.stevens.edu/app/UserHome",
-    },
-  },
-  {
-    category: "Work",
-    color: "green",
-    items: {
-      Linkedin: "https://linkedin.com",
-      Twitter: "https://twitter.com",
-      Leetcode: "https://leetcode.com",
-      Handshake: "https://app.joinhandshake.com/stu/postings",
-      Indeed: "https://www.indeed.com/",
-    },
-  },
-  {
-    category: "Misc",
-    color: "purple",
-    items: {
-      Trakt: "https://trakt.tv/users/peterrauscher/lists",
-      YouTube: "https://www.youtube.com/",
-      Fmovies: "https://fmovies.to/home",
-      Zoro: "https://zoro.to",
-      TempMail: "https://tempmail.email/",
-      // IPTorrents: "https://iptorrents.com/t",
-    },
-  },
+  }
 ];
